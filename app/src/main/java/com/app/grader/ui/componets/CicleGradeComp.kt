4effac969136e662dc2a2ef01947609b6edf913a
Daniel <dacarreno.24@est.ucab.edu.ve>
2025-03-14 09:15:48 -0400

    refactor: Nombre y color de fondo de los componentes circulo

--- app/src/main/java/com/app/grader/ui/componets/CicleGradeComp.kt
+++ app/src/main/java/com/app/grader/ui/componets/CicleGradeComp.kt
@@ -8,44 +8,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.lerp
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import com.app.grader.ui.theme.Error500
-import com.app.grader.ui.theme.Neutral100
-import com.app.grader.ui.theme.Success500
-import com.app.grader.ui.theme.Warning500
-
-fun getColorForGrade(grade: Double): Color {
-    if (grade < 0 || grade > 20) throw IllegalArgumentException("Grade must be between 0 and 20")
-    if (grade == 0.0) return Neutral100
-
-    val colorHigh = Success500
-    val colorMiddle = Warning500
-    val colorLow = Error500
-
-    return when {
-        grade <= 10 -> {
-            val adjustedFraction = (grade / 10f).toFloat()
-            lerp(colorLow, colorMiddle, adjustedFraction)
-        }
-        else -> {
-            val adjustedFraction = ((grade - 10) / 10f).toFloat()
-            lerp(colorMiddle, colorHigh, adjustedFraction)
-        }
-    }
-}
 
 @Composable
-fun CircleGrade(grade: Double, modifier: Modifier = Modifier, strokeWith: Dp = 7.dp, radius : Dp = 40.dp) {
+fun CircleCourse(grade: Double, modifier: Modifier = Modifier, strokeWith: Dp = 7.dp, radius : Dp = 40.dp) {
     if (grade < 0 || grade > 20) throw IllegalArgumentException("Grade must be between 0 and 20")
     if (strokeWith < 0.dp) throw IllegalArgumentException("Stroke width must be positive")
     if (radius < 0.dp) throw IllegalArgumentException("Radius must be positive")
     if (strokeWith > radius) throw IllegalArgumentException("Stroke width must be less than radius")
 
-    val colorBase = MaterialTheme.colorScheme.background
     val colorOnBase = getColorForGrade(grade)
 
     Box(
@@ -56,11 +29,6 @@ fun CircleGrade(grade: Double, modifier: Modifier = Modifier, strokeWith: Dp = 7
                 drawCircle(
                     colorOnBase,
                     radius = radius.toPx(),
-                )
-            }.drawBehind {
-                drawCircle(
-                    colorBase,
-                    radius = (radius - strokeWith).toPx(),
                 )
             }
             .then(modifier)
--- app/src/main/java/com/app/grader/ui/componets/CircleCourseComp.kt
+++ app/src/main/java/com/app/grader/ui/componets/CircleCourseComp.kt
@@ -8,17 +8,44 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.lerp
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.app.grader.ui.theme.Error500
+import com.app.grader.ui.theme.Neutral100
+import com.app.grader.ui.theme.Success500
+import com.app.grader.ui.theme.Warning500
+
+fun getColorForGrade(grade: Double): Color {
+    if (grade < 0 || grade > 20) throw IllegalArgumentException("Grade must be between 0 and 20")
+    if (grade == 0.0) return Neutral100
+
+    val colorHigh = Success500
+    val colorMiddle = Warning500
+    val colorLow = Error500
+
+    return when {
+        grade <= 10 -> {
+            val adjustedFraction = (grade / 10f).toFloat()
+            lerp(colorLow, colorMiddle, adjustedFraction)
+        }
+        else -> {
+            val adjustedFraction = ((grade - 10) / 10f).toFloat()
+            lerp(colorMiddle, colorHigh, adjustedFraction)
+        }
+    }
+}
 
 @Composable
-fun CircleCourse(grade: Double, modifier: Modifier = Modifier, strokeWith: Dp = 7.dp, radius : Dp = 40.dp) {
+fun CircleGrade(grade: Double, modifier: Modifier = Modifier, strokeWith: Dp = 7.dp, radius : Dp = 40.dp) {
     if (grade < 0 || grade > 20) throw IllegalArgumentException("Grade must be between 0 and 20")
     if (strokeWith < 0.dp) throw IllegalArgumentException("Stroke width must be positive")
     if (radius < 0.dp) throw IllegalArgumentException("Radius must be positive")
     if (strokeWith > radius) throw IllegalArgumentException("Stroke width must be less than radius")
 
+    val colorBase = MaterialTheme.colorScheme.surface
     val colorOnBase = getColorForGrade(grade)
 
     Box(
@@ -30,6 +57,11 @@ fun CircleCourse(grade: Double, modifier: Modifier = Modifier, strokeWith: Dp = 
                     colorOnBase,
                     radius = radius.toPx(),
                 )
+            }.drawBehind {
+                drawCircle(
+                    colorBase,
+                    radius = (radius - strokeWith).toPx(),
+                )
             }
             .then(modifier)
     ) {
@@ -37,7 +69,7 @@ fun CircleCourse(grade: Double, modifier: Modifier = Modifier, strokeWith: Dp = 
             if (grade != 0.0) "$grade" else "--",
             style = MaterialTheme.typography.labelMedium,
             textAlign = TextAlign.Center,
-            color = MaterialTheme.colorScheme.background,
+            color = MaterialTheme.colorScheme.onSurface,
         )
     }
 }
