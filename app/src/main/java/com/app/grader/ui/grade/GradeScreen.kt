ae816317d13877521e2b0893aab0cde18a72c7e4
Kobalt09 <136206611+Kobalt09@users.noreply.github.com>
2025-03-14 13:21:01 -0400

    agregado de GradeModel, el GradeCardComp y el cambio en el tipo de dato deGradeEntity
    (cherry picked from commit dfe79df3779342710c8b8671b8cad93c88a2da73)

--- app/src/main/java/com/app/grader/ui/grade/GradeScreen.kt
+++ app/src/main/java/com/app/grader/ui/grade/GradeScreen.kt
@@ -3,12 +3,16 @@ package com.app.grader.ui.grade
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
+import com.app.grader.domain.model.GradeModel
+import com.app.grader.ui.componets.GradeCardComp
 import com.app.grader.ui.componets.HeaderBack
 
 @Composable
@@ -25,6 +29,16 @@ fun GradeScreen(navegateBack: () -> Unit) {
             Spacer(modifier = Modifier.weight(1f))
             Text(text = "Grade SCREEN", fontSize = 25.sp)
             Spacer(modifier = Modifier.weight(1f))
+        }
+    }
+}
+
+@Composable
+fun gradeList(grades: List<GradeModel>, navigateToGrade: () -> Unit) {
+
+    LazyColumn {
+        items(grades) { grade ->
+            GradeCardComp(grade, navigateToGrade)
         }
     }
 }
