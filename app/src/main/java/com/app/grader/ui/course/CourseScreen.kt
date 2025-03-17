8b5190679dd0070d865b60f93c9468bd5a3e6aad
Kobalt09 <136206611+Kobalt09@users.noreply.github.com>
2025-03-17 16:01:19 -0700

    agregado de la descripcion de materias

--- app/src/main/java/com/app/grader/ui/course/CourseScreen.kt
+++ app/src/main/java/com/app/grader/ui/course/CourseScreen.kt
@@ -1,12 +1,13 @@
 package com.app.grader.ui.course
 
+import androidx.compose.foundation.background
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -20,18 +21,20 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.unit.dp
 import com.app.grader.R
-import com.app.grader.domain.model.CourseModel
 import com.app.grader.ui.componets.AddComp
 import com.app.grader.ui.componets.AddCompItem
-import com.app.grader.ui.componets.CourseCardComp
+import com.app.grader.ui.componets.CirclePie
 import com.app.grader.ui.componets.GradeCardComp
 import com.app.grader.ui.componets.HeaderBack
+import com.app.grader.ui.theme.Secondary600
+import com.app.grader.ui.theme.Success500
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.draw.clip
 
 @Composable
 fun CourseScreen(
@@ -59,8 +62,55 @@ fun CourseScreen(
         Column(
             modifier = Modifier.fillMaxSize().padding(innerPadding),
             horizontalAlignment = Alignment.CenterHorizontally) {
-            Spacer(modifier = Modifier.weight(1f))
-            Text(text = "Course SCREEN", fontSize = 25.sp)
+            //Spacer(modifier = Modifier.weight(1f))
+            //Text(text = "Course SCREEN", fontSize = 25.sp)
+            Column(modifier =Modifier
+                .clip(RoundedCornerShape(16.dp))
+                .background(MaterialTheme.colorScheme.surface)
+                .width(300.dp)
+                .padding(15.dp)
+
+            ){
+                //course.value.description
+                Text(text = "Tu promedio", fontSize = 20.sp)
+                Row( modifier = Modifier
+                    .padding(horizontal = 0.dp, vertical = 10.dp)
+
+                ) {
+                    CirclePie()
+                    Column(modifier = Modifier
+                        .padding(horizontal = 10.dp, vertical = 0.dp)
+
+                    ) {
+                        Text(text = "$course.value.description", fontSize = 15.sp)
+                        Text(text = "$course.value.uc UC", fontSize = 15.sp)
+                    }
+                }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .weight(1f)
+                            .align(Alignment.CenterVertically),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(text = "Puntos acumulados", fontSize = 13.sp, color = Success500)
+                        Text(text = "6", fontSize = 15.sp, color = Success500)
+                    }
+                    Column(
+                        modifier = Modifier
+                            .weight(1f)
+                            .align(Alignment.CenterVertically),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(text = "Puntos por evaluar", fontSize = 13.sp, color = Secondary600)
+                        Text(text = "8", fontSize = 15.sp, color = Secondary600)
+                    }
+                }
+
+            }
             Spacer(modifier = Modifier.weight(1f))
             LazyColumn {
                 items(grades.value) { grade ->
