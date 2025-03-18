787378a4078b0bd5043055bbb47b8696627294f0
Daniel <dacarreno.24@est.ucab.edu.ve>
2025-03-18 09:58:20 -0400

    feat: visual de EditGradeScreen

---
+++ app/src/main/java/com/app/grader/ui/componets/EditScreenInputComp.kt
@@ -0,0 +1,37 @@
+package com.app.grader.ui.componets
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun EditScreenInputComp(placeHolderText: String,value: String, onValueChange: (String) -> Unit, leadingIcon: @Composable() (()->Unit)? = null, keyboardOptions: KeyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)) {
+    TextField(
+        colors = TextFieldDefaults.colors().copy(
+            focusedContainerColor = Color.Transparent,
+            unfocusedContainerColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+        ),
+        modifier = Modifier.fillMaxWidth().padding(horizontal = 10.dp),
+        leadingIcon = leadingIcon,
+        placeholder = {
+            Text(placeHolderText, style = MaterialTheme.typography.labelMedium, modifier = Modifier.alpha(0.6f))
+        },
+        keyboardOptions = keyboardOptions,
+        value = value,
+        onValueChange = onValueChange,
+    )
+    HorizontalDivider( modifier = Modifier.alpha(0.5f))
+}
--- app/src/main/java/com/app/grader/ui/editGrade/EditGradeScreen.kt
+++ app/src/main/java/com/app/grader/ui/editGrade/EditGradeScreen.kt
@@ -1,25 +1,40 @@
 package com.app.grader.ui.editGrade
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.repeatOnLifecycle
+import com.app.grader.R
+import com.app.grader.ui.componets.EditScreenInputComp
 import com.app.grader.ui.componets.HeaderBack
 import com.app.grader.ui.editCourse.EditCourseViewModel
 
@@ -44,38 +59,64 @@ fun EditGradeScreen(
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(innerPadding),
+                .padding(innerPadding)
+                .background(MaterialTheme.colorScheme.surface),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "EditCourse SCREEN", fontSize = 25.sp)
-            Spacer(modifier = Modifier.weight(0.2f))
-            Text(text = "Titulo", style = MaterialTheme.typography.labelMedium)
-            TextField(
+            EditScreenInputComp(
+                placeHolderText = "Agregar titulo",
                 value = viewModel.showTitle.value,
                 onValueChange = {
                     viewModel.showTitle.value = it
                     viewModel.title.value = it
+                },
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    capitalization = KeyboardCapitalization.Sentences
+                ),
+                leadingIcon = {
+                    Image(
+                        painter = painterResource(id = R.drawable.cog_outline),
+                        contentDescription = "Ajustes",
+                        colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface),
+                        modifier = Modifier.size(32.dp).padding(end = 5.dp),
+                    )
                 }
             )
-            Text(text = "Descricción", style = MaterialTheme.typography.labelMedium)
-            TextField(
+            EditScreenInputComp(
+                placeHolderText = "Agregar descricción",
                 value = viewModel.showDescription.value,
                 onValueChange = {
                     viewModel.showDescription.value = it
                     viewModel.description.value = it
-                }
+                },
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    capitalization = KeyboardCapitalization.Sentences
+                ),
+                leadingIcon = {
+                    Image(
+                        painter = painterResource(id = R.drawable.cog_outline),
+                        contentDescription = "Ajustes",
+                        colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface),
+                        modifier = Modifier.size(32.dp).padding(end = 5.dp),
+                    )
+                },
             )
-            Text(text = "Calificación", style = MaterialTheme.typography.labelMedium)
-            TextField(
+            EditScreenInputComp(
+                placeHolderText = "Agregar calificación",
                 value = viewModel.showGrade.value,
                 onValueChange = {
                     viewModel.showGrade.value = it
                     viewModel.grade.value.setGrade(it.toIntOrNull() ?: 1)
                 },
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                modifier = Modifier.onFocusChanged { focusState ->
-                    if (!focusState.isFocused) viewModel.showGrade.value = viewModel.grade.value.toString()
-                }
+                leadingIcon = {
+                    Image(
+                        painter = painterResource(id = R.drawable.cog_outline),
+                        contentDescription = "Ajustes",
+                        colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface),
+                        modifier = Modifier.size(32.dp).padding(end = 5.dp),
+                    )
+                },
             )
             Button(onClick = {
                 viewModel.updateOrCreateGrade(gradeId, courseId)
