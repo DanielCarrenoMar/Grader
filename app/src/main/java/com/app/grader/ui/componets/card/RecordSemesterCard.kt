6e08ae326537d3a7aa46b154f7f4bb4d5d917ba3
DanielCarrenoMar <dacarreno.24@est.ucab.edu.ve>
2025-08-29 11:55:10 -0400

    fear: RecordSemesterCard.kt parecida a la de cursos

--- app/src/main/java/com/app/grader/ui/componets/card/RecordSemesterCard.kt
+++ app/src/main/java/com/app/grader/ui/componets/card/RecordSemesterCard.kt
@@ -1,99 +1,150 @@
 package com.app.grader.ui.componets.card
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.platform.LocalWindowInfo
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.app.grader.R
-import com.app.grader.domain.types.Grade
+import com.app.grader.domain.model.SemesterModel
+import com.app.grader.ui.componets.TitleIcon
+import com.app.grader.ui.componets.chart.CircleCourse
 
 @Composable
 fun RecordSemesterCard(
-    title: String,
-    average: Grade,
-    coursesNum: Int,
-    onClick: () -> Unit
+    semester: SemesterModel,
+    onClick: () -> Unit,
+    onDelete: ()-> Unit,
+    onEdit: ()-> Unit,
+    modifier: Modifier = Modifier,
 ){
-    Card(
-        modifier = Modifier.fillMaxWidth().height(180.dp),
-        colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.surface,
-        ),
-        shape = MaterialTheme.shapes.large,
-        onClick = onClick
-    ) {
-        Column(
-            modifier = Modifier.fillMaxSize().padding(horizontal = 16.dp, vertical = 14.dp),
-            verticalArrangement = Arrangement.SpaceBetween
-        ) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    text = title,
-                    style = MaterialTheme.typography.labelMedium,
-                    color = MaterialTheme.colorScheme.primary
-                )
-                Box(
-                    modifier = Modifier
-                        .clip(MaterialTheme.shapes.extraLarge)
-                        .background(MaterialTheme.colorScheme.primary)
-                        .padding(6.dp),
-                    contentAlignment = Alignment.Center
+    var expanded by remember { mutableStateOf(false) }
+    val screenWidth = LocalWindowInfo.current.containerSize.width.dp
+
+    CardContainer(
+        onClick = onClick,
+        modifier = modifier
+    ) { innerPadding ->
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(innerPadding),
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ){
+            Column {
+                TitleIcon(
+                    iconName = "star",
+                    iconId = R.drawable.star,
+                    backgroundColor = MaterialTheme.colorScheme.primary,
                 ) {
-                    Image(
-                        painter = painterResource(id = R.drawable.star),
-                        contentDescription = "fav",
-                        modifier = Modifier,
+                    Text(
+                        text = semester.title,
+                        style = MaterialTheme.typography.labelLarge,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                }
+                Row (
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.padding(top = 27.dp, start = 40.dp)
+                ){
+                    Text(
+                        text = "${semester.size}",
+                        style = MaterialTheme.typography.labelLarge,
+                        color = MaterialTheme.colorScheme.onSurface,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = if (semester.size == 1) " asignatura" else " asignaturas",
+                        modifier = Modifier
+                            .padding(start = 8.dp),
+                        style = MaterialTheme.typography.bodySmall,
+                        color = MaterialTheme.colorScheme.onSurface,
                     )
                 }
             }
-
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 6.dp),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(
-                    text = average.toString(),
-                    style = MaterialTheme.typography.titleMedium,
-                    fontSize = 32.sp,
-                )
-            }
-
             Row (
-                verticalAlignment = Alignment.Bottom
+                modifier = Modifier.weight(1f),
+                verticalAlignment = Alignment.Top,
+                horizontalArrangement = Arrangement.End
             ){
-                Text(
-                    text = coursesNum.toString(),
-                    style = MaterialTheme.typography.labelLarge
-                )
-                Text(
-                    text = if (coursesNum != 1) "  Asignaturas" else "  Asignatura",
-                    style = MaterialTheme.typography.labelSmall,
-                    color = MaterialTheme.colorScheme.onSurfaceVariant
-                )
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier.padding(vertical = 4.dp),
+                ){
+                    CircleCourse(
+                        grade = semester.average,
+                        radius = 33.dp
+                    )
+                }
+            }
+            Row (
+                verticalAlignment = Alignment.Top,
+                horizontalArrangement = Arrangement.End
+            ) {
+                Box(contentAlignment = Alignment.TopEnd){
+                    IconButton(
+                        onClick = { expanded = true },
+                        modifier = Modifier.size(32.dp)
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.dots_vertical_outline),
+                            contentDescription = "edit",
+                            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
+                        )
+                    }
+                    DropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = { expanded = false },
+                        modifier = Modifier.width(200.dp),
+                        offset = DpOffset(x = screenWidth - 200.dp, y = 0.dp),
+                        containerColor = MaterialTheme.colorScheme.surfaceVariant
+                    ) {
+                        DropdownMenuItem(
+                            onClick = { onEdit();expanded = false },
+                            text = {
+                                Text(
+                                    "Editar",
+                                    style = MaterialTheme.typography.labelMedium,
+                                    fontWeight = FontWeight.Medium,
+                                    color = MaterialTheme.colorScheme.primary
+                                )
+                            },
+
+                            )
+                        DropdownMenuItem(onClick = {onDelete();expanded = false},
+                            text = {
+                                Text("Eliminar", style = MaterialTheme.typography.labelMedium,
+                                    fontWeight = FontWeight.Medium, color = MaterialTheme.colorScheme.primary)
+                            }
+                        )
+                    }
+                }
             }
         }
+
     }
 }
