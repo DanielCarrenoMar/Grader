d98e435f047fc44b47a241a5fb0b096d02da255d
DanielCarrenoMar <dacarreno.24@est.ucab.edu.ve>
2025-09-06 09:17:16 -0400

    feat: version 2.0.0 beta 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.app.grader"
         minSdk = 24
         targetSdk = 35
-        versionCode = 13
-        versionName = "2.0.0"
+        versionCode = 14
+        versionName = "2.0.0-beta-5"
         ndk.debugSymbolLevel = "FULL"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
