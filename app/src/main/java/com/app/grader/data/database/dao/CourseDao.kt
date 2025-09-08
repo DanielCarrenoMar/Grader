b85deafd9eb6dfe6f320804814f57fb175d94885
Daniel <dacarreno.24@est.ucab.edu.ve>
2025-09-08 11:07:25 -0400

    refactor: tomar en cuenta las notas que son 0 al calcular promedio del curso

--- app/src/main/java/com/app/grader/data/database/dao/CourseDao.kt
+++ app/src/main/java/com/app/grader/data/database/dao/CourseDao.kt
@@ -19,10 +19,10 @@ interface CourseDao {
     @Query("SELECT * FROM course WHERE id = :courseId")
     suspend fun getCourseFromId(courseId: Int): CourseEntity?
 
-    @Query("SELECT SUM(grade_percentage * percentage) / SUM(percentage) FROM grade WHERE course_id = :courseId AND grade_percentage > 0")
+    @Query("SELECT SUM(grade_percentage * percentage) / SUM(percentage) FROM grade WHERE course_id = :courseId AND grade_percentage >= 0")
     suspend fun getAverageFromCourse(courseId: Int): Double?
 
-    @Query("SELECT SUM(percentage) FROM grade WHERE course_id = :courseId AND grade_percentage > 0")
+    @Query("SELECT SUM(percentage) FROM grade WHERE course_id = :courseId AND grade_percentage >= 0")
     suspend fun getTotalPercentageFromCourse(courseId: Int): Double?
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
